5e76f8aac60eedb2937ce2777a3c45c37c9ed074
Yousaf <112529982+yuzfdev@users.noreply.github.com>
2025-03-11 16:08:21 +0530

    Add files via upload

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User_table,Products_table,Cart_table
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'password')
+    search_fields = ('name', 'email')
+    list_filter = ('name', 'email')
+admin.site.register(User_table, UserAdmin)
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'image')
+    search_fields = ('name', 'description')
+    list_filter = ('description', 'price')
+admin.site.register(Products_table, ProductsAdmin)
